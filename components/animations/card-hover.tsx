8a70b1f97c13ed8e425fe7a46d77c4f5e0dde33d
Aurélien <aurelien.r35@gmail.com>
2023-09-22 10:30:49 +0200

    fix card hover

--- components/animations/card-hover.tsx
+++ components/animations/card-hover.tsx
@@ -15,7 +15,7 @@ const CardHover = ({ image, text, title, link }: CardHoverProps) => {
         <div className=" relative flex justify-between">
           <div className="group relative cursor-pointer">
             <div
-              className="relative z-10 flex flex-col h-[300px] w-[400px] translate-y-[100px] items-center justify-center bg-gray-600 transition-all duration-500 group-hover:translate-y-0 group-hover:bg-green-600
+              className="relative z-10 flex flex-col min-h-[300px] max-w-[400px] translate-y-[100px] items-center justify-center bg-gray-600 transition-all duration-500 group-hover:translate-y-0 group-hover:bg-green-600
   "
             >
               <div className="opacity-100 transition-all duration-500 group-hover:opacity-100 flex flex-col items-center justify-center px-4">
@@ -34,8 +34,8 @@ const CardHover = ({ image, text, title, link }: CardHoverProps) => {
                 </h3>
               </div>
             </div>
-            <div className="relative box-border flex h-[300px] w-[400px] translate-y-[-200px] items-center justify-center bg-white p-5 shadow-[0_20px_50px_rgba(0,0,0,0.8)] transition duration-500 group-hover:translate-y-0  ">
-              <div className="opacity-0 transition-all duration-1000 group-hover:opacity-100 ">
+            <div className="relative box-border flex min-h-[300px] max-w-[400px] translate-y-[-200px] items-center justify-center bg-white p-5 shadow-[0_20px_50px_rgba(0,0,0,0.8)] transition duration-500 group-hover:translate-y-0  opacity-0  group-hover:opacity-100 ">
+              <div className=" ">
                 <p className="relative m-0 p-0 text-center px-2">{text}</p>
                 <Link
                   href={link}
