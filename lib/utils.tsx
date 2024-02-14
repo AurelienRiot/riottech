d46a3f2e7ef2e1cc0b8140f277ea45337bc6369c
Aurélien <aurelien.r35@gmail.com>
2024-02-14 09:42:20 +0100

    currency

--- lib/utils.tsx
+++ lib/utils.tsx
@@ -34,10 +34,27 @@ export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
 
-export const formatter = new Intl.NumberFormat("fr-FR", {
+export const formatter2 = new Intl.NumberFormat("fr-FR", {
   style: "currency",
   currency: "EUR",
+  currencyDisplay: "symbol",
+  minimumFractionDigits: 0,
+  maximumFractionDigits: 2,
 });
+
+export const formatter = (() => {
+  const formatFunction = new Intl.NumberFormat("fr-FR", {
+    style: "currency",
+    currency: "EUR",
+    currencyDisplay: "symbol",
+    minimumFractionDigits: 0,
+    maximumFractionDigits: 2,
+  }).format;
+
+  return {
+    format: (value: number) => formatFunction(value).replace(/\s/g, ""),
+  };
+})();
 
 export const dateFormatter = (date: Date) => {
   return format(date, "d MMMM yyyy", { locale: fr });
