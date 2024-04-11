4f5054898f544fb17bb72fd87296a052a3dc9911
Aurélien <aurelien.r35@gmail.com>
2024-04-11 10:36:29 +0200

    dev

--- app/(routes)/(public)/(autres)/surveillance-elevage/page.tsx
+++ app/(routes)/(public)/(autres)/surveillance-elevage/page.tsx
@@ -2,13 +2,7 @@ import React from "react";
 import Client from "./components/client";
 
 const SurveillanceElevage = async () => {
-  return (
-    <Client
-      imageUrl={
-        "https://res.cloudinary.com/dsztqh0k7/image/upload/v1689341146/clwou7gpxkizqe6d9c9u.png"
-      }
-    />
-  );
+  return <Client imageUrl={"./camera_batiment.jpeg"} />;
 };
 
 export default SurveillanceElevage;
