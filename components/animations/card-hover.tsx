f04cfbb0a1f47df339466ffbe49a8b4085884afd
Aurélien <aurelien.r35@gmail.com>
2024-04-19 11:42:08 +0200

    hover card

--- components/animations/card-hover.tsx
+++ components/animations/card-hover.tsx
@@ -24,34 +24,35 @@ const CardHover = ({
         {...props}
       >
         <div className=" relative flex justify-between">
-          <div className="group relative   transition-all hover:mb-[300px] lg:hover:mb-0 duration-500 ease-in-out">
+          <div className="group relative   transition-all duration-500  ease-in-out hover:mb-[300px]">
             <div
-              className="absolute  inset-0 z-10 flex flex-col min-h-[300px] max-w-[400px] items-center justify-center bg-gray-600 transition-all duration-500  group-hover:bg-green-600  shadow-[0_20px_50px_rgba(0,0,0,0.8)] group-hover:shadow-none
+              className="absolute  inset-0 z-10  min-h-[300px] max-w-[400px]  bg-gray-600 shadow-[0_20px_50px_rgba(0,0,0,0.8)] transition-all  duration-500  group-hover:bg-green-600 group-hover:shadow-none
   "
             >
-              <div className="opacity-100 transition-all duration-500 group-hover:opacity-100 flex flex-col items-center justify-center px-4">
+              <div className="flex h-full flex-col items-center justify-between p-4 opacity-100 transition-all duration-500 group-hover:opacity-100">
                 <Image
                   alt=""
                   width={100}
                   height={100}
                   src={image}
-                  className="max-w-[100px] self-center"
+                  className="max-w-[100px] "
                 />
                 <h3
-                  className="mx-0 font-SourceCodePro mb-0 mt-2.5 p-0 text-center text-[1.5em] text-green-600 group-hover:text-white transition-all duration-500 self-center
+                  className="mx-0 mb-0 mt-2.5  p-0 text-center font-SourceCodePro text-[1.5em] text-green-600 transition-all duration-500 group-hover:text-white
 "
                 >
                   {title}
                 </h3>
+                <p className="">En savoir plus</p>
               </div>
             </div>
-            <div className="relative box-border min-h-[300px] max-w-[400px]   bg-primary-foreground p-5  shadow-[0_20px_50px_rgba(0,0,0,0.8)] transition-all duration-500 group-hover:translate-y-[300px]   opacity-50  group-hover:opacity-100  flex flex-col  justify-between ">
-              <p className="relative m-0 p-0 text-center px-2 text-primary-foreground  group-hover:text-primary transition-all duration-500">
+            <div className="relative box-border flex min-h-[300px]   max-w-[400px] flex-col  justify-between bg-primary-foreground p-5 opacity-50   shadow-[0_20px_50px_rgba(0,0,0,0.8)]  transition-all  duration-500 group-hover:translate-y-[300px]  group-hover:opacity-100 ">
+              <p className="relative m-0 p-0 px-2 text-center text-primary-foreground  transition-all duration-500 group-hover:text-primary">
                 {text}
               </p>
               <Link
                 href={link}
-                className="mx-0 mb-0 mt-[15px] border border-solid border-[#333] p-[5px] font-black text-[#333] self-center transition-all duration-500 group-hover:bg-[#333] group-hover:text-white w-fit
+                className="mx-0 mb-0 mt-[15px] w-fit self-center border border-solid border-[#333] p-[5px] font-black text-[#333] transition-all duration-500 group-hover:bg-[#333] group-hover:text-white
 "
               >
                 En savoir plus
